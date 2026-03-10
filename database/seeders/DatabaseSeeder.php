18b0feeab64c7d741e6736af017592a10f166052
wiryanto33 <indrarencongsakti33@gmail.com>
2026-03-11 00:01:21 +0700

    delete facker/factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,10 +13,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User default
-        User::factory()->create([
-            'name'  => 'admin',
+        // User admin default tanpa factory
+        User::create([
+            'name' => 'admin',
             'email' => 'admin@admin.com',
+            'password' => Hash::make('password'),
         ]);
 
         // Seeder berurutan (dependency order)
